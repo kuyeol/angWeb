af02a0305eb10e6273d23d023da02f2fcc125859
dev <125532184+kuyeol@users.noreply.github.com>
2024-05-18 06:57:58 +0000

    k

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { eroesComponent } from './heroe/heroes.component';
+
+
+const routes: Routes = [
+  { path: 'heroe', component: eroesComponent }
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
