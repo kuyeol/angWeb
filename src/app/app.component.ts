a3687439fabfaffa6ccf8d38fd3c1d2d419350a6
kuyeol <zimop12@gmail.com>
2024-05-19 14:37:21 +0900

    l

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule]
 })
 export class AppComponent {
   title = '[ㅡㅡ ]';
